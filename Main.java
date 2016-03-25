9a99dd99fcc2a219680e3f7d299fddcfd57f1b3c
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-03-25 11:12:23 +0100

    Suppression du println ligne1

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main  {
 	public static void main( String[] args ) {
-		System.out.println("Ligne 1");
 		System.out.println("Hello World");
 		System.out.println("Bye bye");
 		System.out.println("Même pas besoin d'un autre main");
